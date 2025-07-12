f2edbb2125041aeec5a3420277cc802ac0a0aadb
Un8Jordan <laxmirathore554@gmail.com>
2025-07-12 20:41:24 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,7 @@
+print("Friends! enter your names and birthdates here.")
+name=input("1. Enter your name: ")
+birthdate = input("Enter your Birthdate (e.g. 01/01/2000): ")
+
+print("\nHello " + name + " your birthday is on " + birthdate)
+print("Have a great day!")
+print("Your birthdate is stored in the system!")
